e5cdb3ea8a987c21263c429c0438825e246c8f76
helpful-algorithm-9205 <darth.vaper3111991@gmail.com>
2020-10-28 22:31:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ function johnLennonFacts(facts) {
   var array = [];
   var i = 0;
 
-  while (array.length < facts.length) {array.push(facts[i] + `!!!`)}
-  i++;
+  while (array.length < facts.length) {array.push(facts[i] + `!!!`); i++;}
   return array;
 }
