8e76d1df2fe4c037aec7802a0f2e5094bf8ebf1f
helpful-algorithm-9205 <darth.vaper3111991@gmail.com>
2020-10-28 22:52:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function johnLennonFacts(facts){
   }
   return facts
 }
+
+function iLoveTheBeatles(num) {
+  let arr = [];
+  do {
+    arr.push("I love the Beatles!")
+    num++;
+  }
+  while(num < 15);
+  return arr;
+}
