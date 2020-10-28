9a90461225802a935bd5e980a1ef3c8b35cc4d4c
helpful-algorithm-9205 <darth.vaper3111991@gmail.com>
2020-10-28 22:30:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ function theBeatlesPlay(musicians, instruments) {
   for (var i = 0; i < musicians.length; i++) {array.push(musicians[i] + ` plays ` + instruments[i]);}
   return array;
 }
+
+function johnLennonFacts(facts) {
+  var facts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
+  var array = [];
+  var i = 0;
+
+  while (array.length < facts.length) {array.push(facts[i] + `!!!`)}
+  return array;
+}
